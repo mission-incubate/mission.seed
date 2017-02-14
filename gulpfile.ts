1badb1288473e5b2e7586011b575d6b24969a134
Natarajan Ganapathi <natarajanmca11@gmail.com>
2017-02-15 00:34:04 +0530

    mission.tools integration

---
+++ gulpfile.ts
@@ -0,0 +1,20 @@
+import * as gulp from 'gulp';
+
+gulp.task('default', (done: any) => {
+    let api = require('/Users/NatarajanG/BitBucket/mission/mission.seed/node_modules/mission.tools/src/tasks/module.gen');
+    api({
+        userName: 'root',
+        password: 'irtt',
+        database: 'generator',
+        options: {
+            host: 'localhost',
+            port: '3306',
+            dialect: 'mysql',
+            logging: console.warn,
+            force: true,
+            timezone: '+12:00'
+        }
+    }, '/Users/NatarajanG/BitBucket/mission/mission.tools/src/templates/module',
+        '/Users/NatarajanG/BitBucket/mission/mission.seed/src/modules',
+        'AppManager', done);
+});
