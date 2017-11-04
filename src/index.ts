084ed1ed5eef6d725693abf27d2e5e9191c95eb9
Natarajan Ganapathi <natarajanmca11@gmail.com>
2017-11-04 20:10:23 +0530

    packags updated

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export const server = new Bootstrap().server;
 import { AppRouter } from './modules/routes';
 // server.addStaticFileRouting(this.config.webBasePath, __dirname + this.config.webBasePath,
 // this.config.staticFileConfig);
-// server.addStaticFileRouting(this.config.docsBasepath, __dirname + this.config.docsBasepath, 
+// server.addStaticFileRouting(this.config.docsBasepath, __dirname + this.config.docsBasepath,
 // this.config.staticFileConfig);
 server.addApiRouting('/', AppRouter);
 server.start();
