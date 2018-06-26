873886f8a9c69ce79e2d784f22998bab71336597
Natarajan Ganapathi <natarajanmca11@gmail.com>
2018-06-26 22:20:33 +0530

    dotenv dependecy added for environment variable

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import { config, DotenvResult } from 'dotenv';
+const env: DotenvResult = config();
+if (env.error) {
+    throw env.error;
+}
+// tslint:disable-next-line:no-console
+console.log(env.parsed);
+
 import { Bootstrap } from './bootstrap';
 export const server = new Bootstrap().server;
 import { AppRouter } from './routes';
-// server.addStaticFileRouting(this.config.webBasePath, __dirname + this.config.webBasePath,
-// this.config.staticFileConfig);
-// server.addStaticFileRouting(this.config.docsBasepath, __dirname + this.config.docsBasepath,
-// this.config.staticFileConfig);
 server.addApiRouting('/', AppRouter);
 server.start();
